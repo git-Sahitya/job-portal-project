ada973a06b67769d1f549ec70ea0c793814814bb
git-Sahitya <sahitya5289@gmail.com>
2025-02-22 19:38:30 +0530

    feat: update Navbar component to conditionally render login and registration buttons based on user state

--- frontend/src/components/components_lite/Navbar.jsx
+++ frontend/src/components/components_lite/Navbar.jsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -18,45 +19,51 @@ const Navbar = () => {
             <li>Browse</li>
             <li>Job</li>
           </ul>
-
-          <Popover>
-            <PopoverTrigger asChild>
-              <Avatar>
-                <AvatarImage
-                  className="cursor-pointer w-8  rounded-full "
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 ">
-              <div className="flex items-center gap-4 ">
+          {!user ? (
+            <div className="flex items-center gap-2 ">
+              <Button variant="outline">Login</Button>
+              <Button variant="primary">Register</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
                 <Avatar>
                   <AvatarImage
-                    className="cursor-pointer w-16 rounded-full "
+                    className="cursor-pointer w-8  rounded-full "
                     src="https://github.com/shadcn.png"
                     alt="@shadcn"
                   />
                 </Avatar>
-                <div>
-                  <h1 className="font-medium">Sahitya Singh</h1>
-                  <p className="text-sm text-muted-foreground">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
+              </PopoverTrigger>
+              <PopoverContent className="w-80 ">
+                <div className="flex items-center gap-4 ">
+                  <Avatar>
+                    <AvatarImage
+                      className="cursor-pointer w-16 rounded-full "
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h1 className="font-medium">Sahitya Singh</h1>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col text-gray-600">
-                <div className="flex w-fit my-2 items-center gap-2 cursor-pointer">
-                  <User2></User2>
-                  <Button variant="link">Profile</Button>
+                <div className="flex flex-col text-gray-600">
+                  <div className="flex w-fit my-2 items-center gap-2 cursor-pointer">
+                    <User2></User2>
+                    <Button variant="link">Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut></LogOut>
+                    <Button variant="link">Logout</Button>
+                  </div>
                 </div>
-                <div className="flex w-fit items-center gap-2 cursor-pointer">
-                  <LogOut></LogOut>
-                  <Button variant="link">Logout</Button>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
